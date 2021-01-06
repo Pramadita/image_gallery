5265fe1d13727dc813bb49ce9126e52b72b98ae1
Pramadita <pramaditasiede@gmail.com>
2021-01-06 16:48:42 +0700

    upload

--- form.php
+++ form.php
@@ -14,23 +14,55 @@
                 <div class="col-md-12 col-lg-12 mb-4 mb-lg-0" data-aos="fade-up">
 
                     <form action="upload.php" method="post" enctype="multipart/form-data">
-                        <div class="col-md-6 form-group">
-                            <label for="image">1st Art</label>
-                            <input type="file" class="form-control" name="image" id="image" accept="image/*" required />
-                            <div class="validate"></div>
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label for="image">2nd Art</label>
-                            <input type="file" class="form-control" name="image1" id="image" accept="image/*" />
-                            <div class="validate"></div>
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label for="image">3rd Art</label>
-                            <input type="file" class="form-control" name="image2" id="image" accept="image/*" />
-                            <div class="validate"></div>
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <input type="submit" name="submit" class="readmore d-block w-100" value="Send Art Works">
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label for="name">Artist Name</label>
+                                <input type="text" name="artist_name" class="form-control" id="name" data-rule="minlen:4" placeholder="Enter Name" required />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="Sosmed">Social Media</label>
+                                <input type="text" class="form-control" name="Sosmed" id="Sosmed" data-rule="minlen:4" placeholder="Most Famous Social Media from Artist" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <label for="CP">Contact Person or About You (max 250 kata)</label>
+                                <textarea class="form-control" name="CP" cols="20" rows="5" data-rule="required" placeholder="Enter other contact person and your story as artist"></textarea>
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="image">1st Art</label>
+                                <input type="file" class="form-control" name="image" id="image" accept="image/*" required />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <label for="Story1">About 1st Art</label>
+                                <textarea class="form-control" name="Story1" cols="20" rows="5" data-rule="required" placeholder="About the Art" required></textarea>
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="image">2nd Art</label>
+                                <input type="file" class="form-control" name="image1" id="image" accept="image/*" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <label for="Story2">About 2nd Art</label>
+                                <textarea class="form-control" name="Story2" cols="20" rows="5" data-rule="required" placeholder="About the Art" required></textarea>
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="image">3rd Art</label>
+                                <input type="file" class="form-control" name="image2" id="image" accept="image/*" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <label for="Story1">About 3rd Art</label>
+                                <textarea class="form-control" name="Story3" cols="20" rows="5" data-rule="required" placeholder="About the Art" required></textarea>
+                                <div class="validate"></div>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <input type="submit" name="submit" class="readmore d-block w-100" value="Send Art Works">
+                            </div>
                         </div>
                     </form>
                 </div>
--- upload.php
+++ upload.php
@@ -1,4 +1,24 @@
 <?php
+include 'connect.php';
+$nama_file = $_FILES['image']['name'];
+$ukuran_file = $_FILES['image']['size'];
+$tmp_file = $_FILES['image']['tmp_name'];
+
+$nama_file1 = $_FILES['image1']['name'];
+$ukuran_file1 = $_FILES['image1']['size'];
+$tmp_file1 = $_FILES['image1']['tmp_name'];
+
+$nama_file2 = $_FILES['image2']['name'];
+$ukuran_file2 = $_FILES['image2']['size'];
+$tmp_file2 = $_FILES['image2']['tmp_name'];
+
+$artist_name = $_POST['artist_name'];
+$Sosmed = $_POST['Sosmed'];
+$CP = $_POST['CP'];
+$Story1 = $_POST['Story1'];
+$Story2 = $_POST['Story2'];
+$Story3 = $_POST['Story3'];
+
 
 function compressImage($source, $destination, $quality)
 {
@@ -28,7 +48,6 @@ function compressImage($source, $destination, $quality)
     return $destination;
 }
 
-
 // Lokasi path untuk upload
 $uploadPath = "uploads/1/";
 $uploadPath1 = "uploads/2/";
@@ -36,12 +55,15 @@ $uploadPath2 = "uploads/3/";
 
 // ketika melakukan submit file
 $status = $statusMsg = '';
+$fileName = basename($_FILES["image"]["name"]);
+$fileName1 = basename($_FILES["image1"]["name"]);
+$fileName2 = basename($_FILES["image2"]["name"]);
 if (isset($_POST["submit"])) {
     $status = 'error';
     if (!empty($_FILES["image"]["name"])) {
         // File info
-        $fileName = basename($_FILES["image"]["name"]);
-        $imageUploadPath = $uploadPath . $fileName;
+
+        $imageUploadPath = $uploadPath . $fileName1;
         $fileType = pathinfo($imageUploadPath, PATHINFO_EXTENSION);
 
         // Syarat format yang diperbolehkan
@@ -120,6 +142,7 @@ if (isset($_POST["submit"])) {
         $statusMsg = 'Pilih gambar untuk diupload.';
     }
 }
+mysqli_query($koneksi, "insert into artist (artist_name,Sosmed,CP,art1,art2,art3,Story1,Story2,Story3,size1,size2,size3) values ('$artist_name','$Sosmed','$CP','$nama_file','$nama_file1','$nama_file2','$Story1','$Story2','$Story3','$ukuran_file','$ukuran_file1', '$ukuran_file2')");
 
 // Mencetak pesan status
 //echo $statusMsg;
